4ba0dee03860f82de55d96ca978aaea03dc4be22
MzMahmud <moaz.mahmud.64@gmail.com>
2023-03-20 17:23:12 +0600

    shown add/close button on condition

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UiService } from 'src/app/services/ui.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class HeaderComponent implements OnInit {
   showAddForm: boolean = false;
   title: string = 'Task Tracker';
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     uiService.onToggle().subscribe(value => this.showAddForm = value);
   }
 
@@ -20,5 +21,9 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() {
     this.uiService.toggleShowAddForm();
+  }
+
+  hasRoute(url: string): boolean {
+    return this.router.url === url;
   }
 }
