ccbcc87e8fe94a0bff521d5ea948722fa1a4a22e
MzMahmud <moaz.mahmud.64@gmail.com>
2023-03-20 17:09:55 +0600

    added home route to task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,6 +11,10 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+
+const routes: Routes = [
+  { path: '', component: TasksComponent }
+]
 
 @NgModule({
   declarations: [
@@ -25,6 +30,7 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
+    RouterModule.forRoot(routes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
