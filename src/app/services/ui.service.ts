72ca4f3c76fedfce54850f70a82105ee92fe9e44
MzMahmud <moaz.mahmud.64@gmail.com>
2023-03-20 16:54:39 +0600

    added toggle show add form flag in ui service

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/internal/Subject';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
+  private showAddForm: boolean = false;
+  private subject = new Subject<boolean>();
+  
+  constructor() {}
 
-  constructor() { }
+  toggleShowAddForm() {
+    this.showAddForm = !this.showAddForm;
+    this.subject.next(this.showAddForm);
+  }
+
+  onToggle() {
+    return this.subject.asObservable();
+  }
 }
