a6a978531c92c974a8ab6d3fb3d471ba74c99649
MzMahmud <moaz.mahmud.64@gmail.com>
2023-03-22 17:30:35 +0600

    Adding cross-validation to template-driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 import { ForbiddenValidatorDirective } from './shared/forbidden-name.directive';
+import { IdentityRevealedValidatorDirective } from './shared/identity-revealed.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroFormComponent,
-    ForbiddenValidatorDirective
+    ForbiddenValidatorDirective,
+    IdentityRevealedValidatorDirective,
   ],
   imports: [
     BrowserModule,
