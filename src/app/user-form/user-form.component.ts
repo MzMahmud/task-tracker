23970a1e8a4ab00783d666fa5381b8337d81fce8
MzMahmud <moaz.mahmud.64@gmail.com>
2023-03-22 14:58:07 +0600

    Updating parts of the data model

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -21,4 +21,18 @@ export class UserFormComponent {
   onSubmit() {
     console.warn(this.profileForm.value);
   }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+    /*
+    Set a new value for an individual control. The setValue() method
+    strictly adheres to the structure of the form group and replaces
+    the entire value for the control.
+    */
+  }
 }
